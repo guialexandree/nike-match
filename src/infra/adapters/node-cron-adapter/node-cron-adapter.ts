089ad656b16d9d724f9ec36d711f59a097b5ac45
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2023-07-30 18:23:22 -0300

    refactor: melhorias no adapter

--- src/infra/adapters/node-cron-adapter/node-cron-adapter.ts
+++ src/infra/adapters/node-cron-adapter/node-cron-adapter.ts
@@ -1,14 +1,16 @@
 import cron from 'node-cron'
 import env from '@/main/config/env'
 import { Scheduler } from '@/data/protocols'
-import { UpdateProduct } from '@/domain/usecases'
+import { CheckProductAvailability } from '@/domain/usecases'
 
 export class NodeScheduleAdapter implements Scheduler {
-	constructor(private readonly updateProduct: UpdateProduct) {}
+	constructor(private readonly updateProduct: CheckProductAvailability) {}
 	
 	schedule (params: Scheduler.Params): void {
 		const seconds = `*/${params.seconds || env.secondsProductUpdated} * * * * *`
-		cron.schedule(seconds, () => {this.updateProduct.update({ url: params.url }); console.log(global.products)})
+		cron.schedule(seconds, () => {
+			this.updateProduct.checkAvailability({ url: params.url })
+		})
 	}
 }
 
