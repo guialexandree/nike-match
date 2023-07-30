5e7c0e8319540a7ad5ac386035c46028b61cd163
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2023-07-30 20:27:08 -0300

    refactor: melhorias no app

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,12 +1,18 @@
 require('module-alias/register')
+import { Sqlite3Helper } from '@/infra/db'
 import { setupApp } from '@/main/config/app'
 import env from '@/main/config/env'
 
 try {
-	setupApp()
-	.listen(env.port, () => {
+	Sqlite3Helper.connect()
+	Sqlite3Helper.seeds()
+	
+	const app = setupApp()
+
+	app.listen(env.port, () => {
 		console.log(`Server running at port ${env.port}`)
 	})
+	
 } catch (error) {
 	console.error(error)
 }
