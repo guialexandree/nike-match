ef85050167ac56b7b6f0e27035132acbf84a85ff
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2023-07-30 20:24:07 -0300

    fix: corrige parâmetro do agendador

--- src/infra/adapters/node-cron-adapter/node-cron-adapter.ts
+++ src/infra/adapters/node-cron-adapter/node-cron-adapter.ts
@@ -1,15 +1,14 @@
 import cron from 'node-cron'
-import env from '@/main/config/env'
 import { Scheduler } from '@/data/protocols'
-import { CheckProductAvailability } from '@/domain/usecases'
+import env from '@/main/config/env'
 
 export class NodeScheduleAdapter implements Scheduler {
-	constructor(private readonly updateProduct: CheckProductAvailability) {}
+	constructor(private readonly seconds?: number) {}
 	
-	schedule (params: Scheduler.Params): void {
-		const seconds = `*/${params.seconds || env.secondsProductUpdated} * * * * *`
+	schedule (job: Scheduler.Params): void {
+		const seconds = `*/${this.seconds || env.secondsProductUpdated} * * * * *`
 		cron.schedule(seconds, () => {
-			this.updateProduct.checkAvailability({ url: params.url })
+			job()
 		})
 	}
 }
