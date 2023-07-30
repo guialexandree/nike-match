0add1c40a15e2c72a46b08ad41cd7364befdd7ad
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2023-07-30 20:27:34 -0300

    feat: adiciona envio de mensagem via whatsapp no app

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,10 +1,30 @@
 import express, { type Express } from 'express'
-import setupRoutes from '../config/router'
-import setupServices from '../config/services'
+import { Sqlite3Helper } from '@/infra/db'
+import { WhatsAppAdapter } from '@/infra/senders'
+import setupRoutes from '@/main/config/router'
+import { makeCheckProductAvailabilityFactory } from '@/main/factories/usecases'
+import { makeSchedulerAdapter } from '@/main/factories/adapters'
 
 export const setupApp = (): Express => {
 	const app = express()
+	
+	const whatsAppAdapter = new WhatsAppAdapter()
+	whatsAppAdapter.on('start-get-products', () => startGetProducts(whatsAppAdapter))
+
 	setupRoutes(app)
-	setupServices()
+
 	return app
+}
+
+const startGetProducts = (whatsAppAdapter: WhatsAppAdapter): void => {
+	const scheduler = makeSchedulerAdapter()
+		const products = Sqlite3Helper.getAllProducts()
+	
+		setTimeout(() => {
+			console.log('Iniciou o serviço de busca de produtos')
+			products.map(product => {
+				const updateProduct = makeCheckProductAvailabilityFactory(whatsAppAdapter)
+				scheduler.schedule(() => updateProduct.checkAvailability({ url: product.url }))
+			})
+		}, 100)
 }
