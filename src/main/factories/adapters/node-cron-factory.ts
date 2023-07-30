ee7ce9c94d906958f337df025ed647ece2433dd6
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2023-07-30 20:26:05 -0300

    fix: correções no factory

--- src/main/factories/adapters/node-cron-factory.ts
+++ src/main/factories/adapters/node-cron-factory.ts
@@ -1,8 +1,6 @@
 import { Scheduler } from '@/data/protocols'
 import { NodeScheduleAdapter } from '@/infra/adapters'
-import { makeUpdateProductFactory } from '@/main/factories/usecases'
 
-export const makeScheduler = (): Scheduler => {
-	const updateProduct = makeUpdateProductFactory()
-	return new NodeScheduleAdapter(updateProduct)
+export const makeSchedulerAdapter = (): Scheduler => {
+	return new NodeScheduleAdapter()
 }
